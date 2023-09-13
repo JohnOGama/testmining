c59c3b9720cb6fae989810b3bccd47e89961d8d5
xDev2022-power <22comzace@gmail.com>
2023-09-13 21:45:46 +0800

    removed node_modules folder

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'Test Project',
+  description: 'Page Description Here',
 }
 
 import Hero from '@/components/hero'
