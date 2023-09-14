c41d060448fa4ea58eb50141c763f4018a6dc875
xDev2022-power <22comzace@gmail.com>
2023-09-14 21:25:15 +0800

    fdgfg

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,29 +1,33 @@
 import Link from 'next/link'
 import MobileMenu from './mobile-menu'
-import logo from "../../app/src/assets/images/orca.png"
+import logo from "../../app/src/assets/images/OrcaLogoHeader.png"
 
 import Image from "next/image";
 import "@/app/css/Background.module.css"
 
 export default function Header() {
   return (
-    <header className="absolute w-full z-30">
-      <div className="max-w-6sm mx-auto px-6 sm:px-8">
-        <div className="flex items-left justify-between h-20">
+    <header className="absolute w-50 z-30">
+      <div className="max-w-8sm mx-auto px-2 sm:px-6">
+        <div className="flex items-center h-30">
 
           {/* Site branding */}
           <div className="shrink-0 mr-4">
             {/* Logo */}
             <Link href="/" className="block" aria-label="Cruip">
-           <Image className="h-20 mix-blend-multiply" src={logo} alt={""} />
-            
+           <Image className="h-20 mix-blend-multiply" src={logo} alt={""} style={{display: 'flex'}}/>
+              
+              
             </Link>
+           
           </div>
 
           {/* Desktop navigation */}
           <nav className="hidden md:flex md:grow">
             {/* Desktop sign in links */}
             <ul className="flex grow justify-end flex-wrap items-center">
+              
+              {/*
               <li>
                 <Link
                   href="/signin"
@@ -32,9 +36,41 @@ export default function Header() {
                   Sign in
                 </Link>
               </li>
+
               <li>
-                <Link href="/signup" className="btn-sm text-white bg-purple-600 hover:bg-purple-700 ml-3">
+                <Link href="/signup" className=" text-black hover:text-orange-600 ml-4">
                   Sign up
+                </Link>
+              </li>
+              <li>
+                <Link href="/signup" className=" text-black hover:text-orange-600 ml-4">
+                  ABOUT US
+                </Link>
+              </li>
+              <li>
+                <Link href="/signup" className=" text-black hover:text-orange-600 ml-4">
+                  SUSTAINABILITY
+                </Link>
+              </li>
+  */}
+              <li>
+                <Link href="/signup" className=" text-black hover:text-orange-600 ml-4">
+                  CORPORATE GOVERNANCE
+                </Link>
+              </li>
+              <li>
+                <Link href="/signup" className=" text-black hover:text-orange-600 ml-4">
+                  CORPORATE GOVERNANCE
+                </Link>
+              </li>
+              <li>
+                <Link href="/signup" className=" text-black hover:text-orange-600 ml-4">
+                  COMPANY DISCLOSURE
+                </Link>
+              </li>
+              <li>
+                <Link href="/signup" className=" text-black hover:text-orange-600 ml-4">
+                  INVESTOR RELATIONS
                 </Link>
               </li>
             </ul>
