664cd14a7a7d8ffe8d4514ae58d972507a53ab2b
xDev2022-power <22comzace@gmail.com>
2023-09-14 14:20:09 +0800

    dsf

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -10,38 +10,33 @@ import Zigzag from '@/components/zigzag'
 import Testimonials from '@/components/testimonials'
 
 import styles from '../app/css/Background.module.css'
-import background from "../src/assets/images/homeDefaultBg.png"
+import background from "../../app/src/assets/images/homeDefaultBg.jpg"
+// "../src/assets/images/homeDefaultBg.png" 
 //'../../src/assets/images/features-03-image-03.png'
 
 export default function Home() {
 
-  const myStyle={
+  const videoStyle={
     backgroundImage:
     `url(${background.src})`,
     //`url("https://img.freepik.com/free-photo/wide-angle-shot-singletree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg")`,
-    height:'100vh',
+    height:'80vh',
     
     marginTop:'-70px',
     fontSize:'50px',
-    backgroundSize: 'cover',
+   // backgroundSize: 'cover',
+    backgroundSize: '1600px 1200px',
     backgroundRepeat: 'no-repeat',
-};
+  };
 
   return (
     
-    <div  style= {myStyle}>
-      <div
-      style={{
-        display: 'flex',
-        alignItems: 'left',
-        justifyContent: 'left',
-      }} 
-      >
+    <div  >
       <Hero />
-      </div>
       
    
       {/*
+      style= {videoStyle}
 sdfdsf
        style={{
         display: 'flex',
