a911ff5cc31735ba520af8507b0d707b0fbc313d
xDev818 <143838286+xDev818@users.noreply.github.com>
2023-09-14 22:33:27 +0800

    Update mobile-menu.tsx

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -59,7 +59,7 @@ export default function MobileMenu() {
         className="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out"
         style={mobileNavOpen ? { maxHeight: mobileNav.current?.scrollHeight, opacity: 1 } : { maxHeight: 0, opacity: 0.8 }}
       >
-        <ul className="bg-gray-800 px-4 py-2">
+       <ul className="text-black hover:text-orange-600 px-4 py-2">
 
           {/*
           <li>
