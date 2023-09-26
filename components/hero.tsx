adf9d0b8f747e1ccf399be34cf72acc9af5cffe3
xDev2022-power <22comzace@gmail.com>
2023-09-26 15:58:21 +0800

    0926 358pm

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import VideoThumb from '@/public/images/hero-image-01.jpg'
 
 import ModalVideo from '@/components/modal-video'
 
@@ -20,12 +19,20 @@ export default function Hero() {
             <h1 className='text-5xl' >Let's talk your requirement</h1>
             <p className="text-6sm text-gray-800 mb-8 " >Mining , Construction and Waste Management Industry is our expertise. Making the complex simple.</p>
             <div className="text-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
-              <div  className="flex">
-                <a className="btn text-white bg-gray-800 hover:bg-orange-500 w-30 " href="#0">
-                <Image src={contactus} alt={""} style={{alignContent:'normal',alignItems:'left', width: '50px',height:'50px'}}> 
-                </Image> 
-                 (+63) 915 1234567 
+              <div  className="relative">
+                <a className="btn text-white bg-gray-500 w-22 h-20 " href="#0" style={{backgroundColor:'#08c5d6'}}>
+                  {/* 
+                  btn text-white bg-gray-800 hover:bg-orange-500 w-30 
+                  style={{alignContent:'normal',alignItems:'left', width: '50px',height:'50px'}}
+                  */}
+                <Image src={contactus} alt={""} style={{alignContent:'normal',textAlign: 'left',
+                 alignItems:'left', width: '50px',height:'50px'}} > 
+                </Image>
+                <div className="text-2xl text-black md:pb-15">
+                (+63) 915 1234567 
                  <br></br> (+63) 915 8888888
+                </div> 
+                
                 </a> 
                 
               </div>
@@ -33,15 +40,16 @@ export default function Hero() {
               
             </div>
           </div>
-          <ModalVideo
-            thumb={HomeBgEmail}
-            thumbWidth={600}
-            thumbHeight={600}
-            thumbAlt="Modal video thumbnail"
-            video="/videos/Mining.mp4"
-            videoWidth={2000}
-            videoHeight={1080} />
-            
+          <div className="modalvideomain">
+            <ModalVideo
+              thumb={HomeBgEmail}
+              thumbWidth={600}
+              thumbHeight={600}
+              thumbAlt="Modal video thumbnail"
+              video="/videos/Mining.mp4"
+              videoWidth={2000}
+              videoHeight={1080} />
+          </div>
         </div>
         
       </div>
