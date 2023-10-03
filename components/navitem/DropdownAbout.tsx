16976559b22b15aaf3dd142eded321bb0013d341
John Ogama <jogama696@gmail.com>
2023-10-03 14:57:47 +0800

    feat: fix header menu

--- components/navitem/DropdownAbout.tsx
+++ components/navitem/DropdownAbout.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { aboutDropdown } from "./NavItem";
 //import { Link } from "react-router-dom";
-import Link from 'next/link'
+import Link from "next/link";
 import "./DropdownAbout.css";
 
 function DropdownAbout() {
@@ -9,29 +9,26 @@ function DropdownAbout() {
 
   return (
     <>
-
       <ul
-        className={dropdown ?  "about-submenu active" : "about-submenu"}
+        className={dropdown ? "about-submenu active" : "about-submenu"}
         onClick={() => setDropdown(!dropdown)}
       >
-        {
-            aboutDropdown.map((item) => {
-              {console.log(item.cName)}
-              return (
-                <li key={item.id}>
-                  <Link
-                    href={item.path}
-                    className={item.cName}
-                    onClick={() => setDropdown(false)}
-                  >
-                    {item.title}
-                  </Link>
-                </li>
-              );
-            })
-          
-       }
-      
+        {aboutDropdown.map((item) => {
+          {
+            console.log(item.cName);
+          }
+          return (
+            <li key={item.id}>
+              <Link
+                href={item.path}
+                className={item.cName}
+                onClick={() => setDropdown(false)}
+              >
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
